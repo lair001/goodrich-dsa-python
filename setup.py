c6cb349533f6cc09d20813d4a6d74ec0000406e1
Samuel Lair <lair001@gmail.com>
2018-07-07 22:33:16 -0400

    Replace PYTHONPATH management ...
    with a solution based on pipenv and setuptools.
    Setuptools is used to define a package from this
    package.  This package is then installed into
    the virtual environment via pipenv.  A
    convenience script run is provided to shorten
    the commands to execute the exercises.  The
    PYTHONPATH management scripts have been removed.

---
+++ setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+setuptools.setup(
+    name = "exercises",
+    author = "Samuel Lair",
+    author_email = "lair001@gmail.com",
+    version = "0.0.1",
+    packages=setuptools.find_packages())
