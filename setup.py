e846fae3f63cb6cce9f9e603b94a02160589326b
Samuel Lair <lair001@gmail.com>
2018-07-08 04:29:33 -0400

    Revise project package metadata

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 import setuptools
 
 setuptools.setup(
-    name = "exercises",
+    name = "data-structures-and-algorithms-in-python-solutions",
     author = "Samuel Lair",
     author_email = "lair001@gmail.com",
     version = "0.0.1",
-    packages=setuptools.find_packages())
+    license = "MIT",
+    url = "https://github.com/lair001/data-structures-and-algorithms-in-python-solutions",
+    description = ("My solutions to Data Structures and Algorithms in Python by "
+                   "Michael T. Goodrich, Roberto Tamassia, and Michael H. Goldwasser"),
+    packages = setuptools.find_packages()
+)
