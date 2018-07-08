1859c970cfa41c501b00c76f976028c1bb719a29
Samuel Lair <lair001@gmail.com>
2018-07-07 22:35:11 -0400

    Implement an argv utility function ...
    for parsing a sequence of integers.

---
+++ exercises/argv_utils.py
@@ -0,0 +1,34 @@
+import sys, re
+
+def parse_int_seq(beginning_index):
+    seq = []
+    current_index = beginning_index
+    opening = None
+    closing = None
+    openings = ['[', '{', '(']
+    openings_to_closings = {
+        '[' : ']',
+        '{' : '}',
+        '(' : ')'
+    }
+    blanks = [" ", ""]
+
+    while True:
+        arg = sys.argv[current_index]
+        clean_arg = arg
+        current_index += 1
+        if opening == None:
+            if arg[0] in openings:
+                opening = arg[0]
+                clean_arg = re.sub("\A%s" %(re.escape(opening)), '', clean_arg)
+                closing = openings_to_closings[opening]
+                cleaning_pattern = "(,|%s\Z)" %(re.escape(closing))
+            else:
+                continue
+        clean_arg = re.sub(cleaning_pattern, ' ', clean_arg)
+        for token in clean_arg.split(" "):
+            if token not in blanks:
+                seq.append(int(token))
+        if arg[-1] == closing:
+            break
+    return seq, current_index
