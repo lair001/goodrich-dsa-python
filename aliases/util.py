038016162ddf480de82dd3c774d1de2ddfc1f3bb
Samuel Lair <lair001@gmail.com>
2018-07-15 22:34:15 -0400

    Rework solution aliasing API

--- aliases/util.py
+++ aliases/util.py
@@ -1,6 +1,6 @@
 from aliases.ch1 import ch1_aliases
 
-all = dict(**ch1_aliases)
-
-def substitute(argv_1):
-    return all.get(argv_1, argv_1)
+def get_alias_value(alias):
+    return dict(
+                **ch1_aliases
+            ).get(alias, alias)
