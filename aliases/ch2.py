fbd18355ab531f80751d0b72e0a1543fd5e65e59
Samuel Lair <lair001@gmail.com>
2018-07-17 00:22:46 -0400

    Implement exercise R-2.9

---
+++ aliases/ch2.py
@@ -0,0 +1,5 @@
+from os import path as os_path
+
+ch2_aliases = {
+    "ch2_ex9_sc1" : os_path.join("solutions", "exercises", "R", "ch2", "ex9", "vector_subtraction")
+}
--- aliases/util.py
+++ aliases/util.py
@@ -1,6 +1,8 @@
 from aliases.ch1 import ch1_aliases
+from aliases.ch2 import ch2_aliases
 
 def get_alias_value(alias):
     return dict(
-                **ch1_aliases
+                **ch1_aliases,
+                **ch2_aliases
             ).get(alias, alias)
