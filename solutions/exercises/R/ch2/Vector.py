fe82db493d0628252f59b52daa74f7650f4b93ca
Samuel Lair <lair001@gmail.com>
2018-07-16 23:30:03 -0400

    Add Vector class of section 2.3.3

---
+++ solutions/exercises/R/ch2/Vector.py
@@ -0,0 +1,42 @@
+# This is the Vector class of section 2.3.3 that is referenced in the exercises.  The vendor class (see vendor
+# directory) has additional functionality, some of which is asked for in the exercises.
+
+class Vector:
+    """Represent a vector in a multidimensional space."""
+
+    def __init__(self, d):
+        """Create d-dimensional vector of zeros."""
+        self._coords = [0] * d
+
+    def __len__(self):
+        """Return the dimension of the vector."""
+        return len(self._coords)
+
+    def __getitem__(self, j):
+        """Return jth coordinate of vector."""
+        return self._coords[j]
+
+    def __setitem__(self, j, val):
+        """Set jth coordinate of vector to given value."""
+        self._coords[j] = val
+
+    def __add__(self, other):
+        """Return sum of two vectors."""
+        if len(self) != len(other): # relies on len method
+            raise ValueError('dimensions must agree')
+        result = Vector(len(self)) # start with vector of zeros
+        for j in range(len(self)):
+            result[j] = self[j] + other[j]
+        return result
+
+    def __eq__(self, other):
+        """Return True if vector has same coordinates as other."""
+        return self._coords == other._coords
+
+    def __ne__(self, other):
+        """Return True if vector differs from other."""
+        return not self == other # rely on existing eq definition
+
+    def __str__(self):
+        """Produce string representation of vector."""
+        return '<' + str(self._coords)[1:−1] + '>' # adapt list representation
