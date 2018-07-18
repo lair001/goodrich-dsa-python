1e8606e1383d216e9be979d87335bdfce5135134
Samuel Lair <lair001@gmail.com>
2018-07-18 00:03:45 -0400

    Make ch2_aliases dict PEP 8 compliant

--- aliases/ch2.py
+++ aliases/ch2.py
@@ -1,6 +1,6 @@
 from os import path as os_path
 
 ch2_aliases = {
-    "ch2_ex9_sc1" : os_path.join("solutions", "exercises", "R", "ch2", "ex9", "vector_subtraction"),
-    "ch2_ex10_sc1" : os_path.join("solutions", "exercises", "R", "ch2", "ex10", "vector_negation")
+    "ch2_ex9_sc1": os_path.join("solutions", "exercises", "R", "ch2", "ex9", "vector_subtraction"),
+    "ch2_ex10_sc1": os_path.join("solutions", "exercises", "R", "ch2", "ex10", "vector_negation")
 }
