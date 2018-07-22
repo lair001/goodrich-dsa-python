13737b0662bdc1ea726b2faa2024df65f188eaf2
Samuel Lair <lair001@gmail.com>
2018-07-22 19:17:18 -0400

    Implement exercise C-2.25

--- aliases/ch2.py
+++ aliases/ch2.py
@@ -7,5 +7,7 @@ ch2_aliases = {
     "ch2_ex12_sc1": os_path.join("solutions", "exercises", "R", "ch2", "ex12", "vector_multiplication"),
     "ch2_ex13_sc1": os_path.join("solutions", "exercises", "R", "ch2", "ex13", "vector_righthand_multiplication"),
     "ch2_ex14_sc1": os_path.join("solutions", "exercises", "R", "ch2", "ex14", "dot_product"),
-    "ch2_ex15_sc1": os_path.join("solutions", "exercises", "R", "ch2", "ex15", "construct_vector_from_iterable")
+    "ch2_ex15_sc1": os_path.join("solutions", "exercises", "R", "ch2", "ex15", "construct_vector_from_iterable"),
+
+    "ch2_ex25_sc1": os_path.join("solutions", "exercises", "C", "ch2", "ex25", "poly_vect_mul")
 }
