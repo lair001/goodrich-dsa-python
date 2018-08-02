186e0bb7548c3b86136dca3cfd48258a5742194f
Samuel Lair <lair001@gmail.com>
2018-08-01 22:06:53 -0400

    Add Sequence class of Section 2.4.3

---
+++ solutions/fragments/ch2/Sequence.py
@@ -0,0 +1,36 @@
+# This is the Sequence class of Code Fragment 2.14 in Section 2.4.3 that is referenced in the exercises.
+
+from abc import ABCMeta, abstractmethod                 # need these definitions
+
+class Sequence(metaclass=ABCMeta):
+    """Our own version of collections.Sequence abstract base class."""
+
+    @abstractmethod
+    def __len__(self):
+        """Return the length of the sequence."""
+
+    @abstractmethod
+    def __getitem__(self, j):
+        """Return the element at index j of the sequence."""
+
+    def __contains__(self, val):
+        """Return True if val found in the sequence; False otherwise."""
+        for j in range(len(self)):
+            if self[j] == val:                          # found match
+                return True
+        return False
+
+    def index(self, val):
+        """Return leftmost index at which val is found (or raise ValueError)."""
+        for j in range(len(self)):
+            if self[j] == val:                          # leftmost match
+                return j
+        raise ValueError('value not in sequence')       # never found a match
+
+    def count(self, val):
+        """Return the number of elements equal to given value."""
+        k = 0
+        for j in range(len(self)):
+            if self[j] == val:                          # found a match
+                k += 1
+        return k
