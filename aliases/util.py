4efe487b582775eb09f7fafd1d083fc176113a55
Samuel Leighton Lair <lair001@gmail.com>
2022-07-23 14:34:24 -0400

    Rework how aliases are defined

--- aliases/util.py
+++ aliases/util.py
@@ -1,30 +1,49 @@
-import string
+import glob
 
-from aliases.ch1 import ch1_aliases
-from aliases.ch2 import ch2_aliases
 from os import linesep as newline
 from os import path as os_path
 
-def get_alias_value(alias):
-    return dict(
-                **ch1_aliases,
-                **ch2_aliases
-            ).get(alias, alias)
+aliases = {}
+
+def get_alias_path(alias):
+    chapter = alias.split("_")[0]
+    load_aliases(chapter)
+
+    return aliases[chapter][alias]
 
 def get_alias_list(chapter):
-    match chapter:
-        case "ch1":
-            aliases = ch1_aliases
-        case "ch2":
-            aliases = ch2_aliases
-        case _:
-            aliases = dict(
-                **ch1_aliases,
-                **ch2_aliases
-            )
+    load_aliases(chapter)
 
     result = ""
-    for key, val in aliases.items():
-        result += "{:14s} {:s}".format(key, os_path.basename(val)) + newline
+    for chapter_aliases in aliases.values():
+        for alias, path in chapter_aliases.items():
+            result += "{:14s} {:s}".format(alias, os_path.basename(path)) + newline
 
     return result
+
+def add_alias(chapter, exercise, filename):
+    if chapter not in aliases.keys():
+        aliases[chapter] = {}
+
+    i = 1
+    while True:
+        alias = "{}_{}_sc{}".format(chapter, exercise, i)
+        if alias not in aliases[chapter].keys():
+            aliases[chapter][alias] = os_path.join("solutions", "exercises", chapter, exercise, filename)
+            return alias
+        else:
+            i += 1
+
+def load_aliases(chapter):
+    if len(chapter) < 3:
+        chapter = "*"
+    paths = glob.glob(os_path.join("aliases", chapter + "_def.txt"))
+
+    for path in paths:
+        def_file = open(path, "r")
+        chapter = os_path.basename(path).split("_")[0]
+        for line in def_file:
+            line.strip()
+            if len(line) > 1:
+                exercise, filename = line.split()
+                add_alias(chapter, exercise, filename)
