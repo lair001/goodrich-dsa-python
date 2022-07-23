a96fec6eac8d96fa798cb8dbc22ee35e28e15933
Samuel Leighton Lair <lair001@gmail.com>
2022-07-23 11:43:56 -0400

    Add script to list aliases

--- aliases/util.py
+++ aliases/util.py
@@ -1,8 +1,30 @@
+import string
+
 from aliases.ch1 import ch1_aliases
 from aliases.ch2 import ch2_aliases
+from os import linesep as newline
+from os import path as os_path
 
 def get_alias_value(alias):
     return dict(
                 **ch1_aliases,
                 **ch2_aliases
             ).get(alias, alias)
+
+def get_alias_list(chapter):
+    match chapter:
+        case "ch1":
+            aliases = ch1_aliases
+        case "ch2":
+            aliases = ch2_aliases
+        case _:
+            aliases = dict(
+                **ch1_aliases,
+                **ch2_aliases
+            )
+
+    result = ""
+    for key, val in aliases.items():
+        result += "{:14s} {:s}".format(key, os_path.basename(val)) + newline
+
+    return result
