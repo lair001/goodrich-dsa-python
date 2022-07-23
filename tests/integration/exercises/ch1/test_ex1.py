1cbbec1886b181f6ef2cb5ff71aa76ab32902912
Samuel Leighton Lair <lair001@gmail.com>
2022-07-23 06:09:57 -0400

    Fix integration tests

--- tests/integration/exercises/ch1/test_ex1.py
+++ tests/integration/exercises/ch1/test_ex1.py
@@ -1,27 +1,27 @@
 from os import path as os_path
+from os import linesep as newline
 from tests.libs.utils.shell import get_stdout_for_cmd
 
 
 def test_using_modulo_when_multiple():
-    cmd = [os_path.join("solutions", "exercises", "R", "ch1", "ex1", "using_modulo"), "4", "2"]
-    print(cmd)
+    cmd = ["python", os_path.join("solutions", "exercises", "ch1", "ex1", "using_modulo"), "4", "2"]
     result = get_stdout_for_cmd(cmd)
-    assert result == "4 is a multiple of 2.\n"
+    assert result == "4 is a multiple of 2." + newline
 
 
 def test_using_modulo_when_not_multiple():
-    cmd = [os_path.join("solutions", "exercises", "R", "ch1", "ex1", "using_modulo"), "5", "3"]
+    cmd = ["python", os_path.join("solutions", "exercises", "ch1", "ex1", "using_modulo"), "5", "3"]
     result = get_stdout_for_cmd(cmd)
-    assert result == "5 is not a multiple of 3.\n"
+    assert result == "5 is not a multiple of 3." + newline
 
 
 def test_without_modulo_when_multiple():
-    cmd = [os_path.join("solutions", "exercises", "R", "ch1", "ex1", "without_modulo"), "4", "2"]
+    cmd = ["python", os_path.join("solutions", "exercises", "ch1", "ex1", "without_modulo"), "4", "2"]
     result = get_stdout_for_cmd(cmd)
-    assert result == "4 is a multiple of 2.\n"
+    assert result == "4 is a multiple of 2." + newline
 
 
 def test_without_modulo_when_not_multiple():
-    cmd = [os_path.join("solutions", "exercises", "R", "ch1", "ex1", "without_modulo"), "5", "3"]
+    cmd = ["python", os_path.join("solutions", "exercises", "ch1", "ex1", "without_modulo"), "5", "3"]
     result = get_stdout_for_cmd(cmd)
-    assert result == "5 is not a multiple of 3.\n"
+    assert result == "5 is not a multiple of 3." + newline
