9bd7b0dcb042b4a612ac0bedd9437d5077dc8a5a
Samuel Leighton Lair <lair001@gmail.com>
2022-07-31 00:19:27 -0400

    Add solution for ex 3.24

---
+++ solutions/exercises/ch3/ex24/discussion.md
@@ -0,0 +1,4 @@
+Function example2 can be found in exercises.py located in the vendor files for ch03.
+
+Let us count array reads and addition as our primitive operations. We read every other element once to add it to our
+running total. Hence, there are $n$ array reads and example2 is $O(n)$.
--- solutions/vendor/ch03/exercises.py
+++ solutions/vendor/ch03/exercises.py
@@ -20,49 +20,53 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 def example1(S):
-  """Return the sum of the elements in sequence S."""
-  n = len(S)
-  total = 0
-  for j in range(n):             # loop from 0 to n-1
-    total += S[j]
-  return total
+    """Return the sum of the elements in sequence S."""
+    n = len(S)
+    total = 0
+    for j in range(n):  # loop from 0 to n-1
+        total += S[j]
+    return total
+
 
 def example2(S):
-  """Return the sum of the elements with even index in sequence S."""
-  n = len(S)
-  total = 0
-  for j in range(0, n, 2):       # note the increment of 2
-    total += S[j]
-  return total
-  
+    """Return the sum of the elements with even index in sequence S."""
+    n = len(S)
+    total = 0
+    for j in range(0, n, 2):  # note the increment of 2
+        total += S[j]
+    return total
+
+
 def example3(S):
-  """Return the sum of the prefix sums of sequence S."""
-  n = len(S)
-  total = 0
-  for j in range(n):            # loop from 0 to n-1
-    for k in range(1+j):        # loop from 0 to j
-      total += S[k]
-  return total
+    """Return the sum of the prefix sums of sequence S."""
+    n = len(S)
+    total = 0
+    for j in range(n):  # loop from 0 to n-1
+        for k in range(1 + j):  # loop from 0 to j
+            total += S[k]
+    return total
+
 
 def example4(S):
-  """Return the sum of the prefix sums of sequence S."""
-  n = len(S)
-  prefix = 0
-  total = 0
-  for j in range(n):
-    prefix += S[j]
-    total += prefix
-  return total
-
-def example5(A, B):           # assume that A and B have equal length
-  """Return the number of elements in B equal to the sum of prefix sums in A."""
-  n = len(A)                  
-  count = 0
-  for i in range(n):          # loop from 0 to n-1
+    """Return the sum of the prefix sums of sequence S."""
+    n = len(S)
+    prefix = 0
     total = 0
-    for j in range(n):        # loop from 0 to n-1
-      for k in range(1+j):    # loop from 0 to j
-        total += A[k]
-    if B[i] == total:
-      count += 1
-  return count
+    for j in range(n):
+        prefix += S[j]
+        total += prefix
+    return total
+
+
+def example5(A, B):  # assume that A and B have equal length
+    """Return the number of elements in B equal to the sum of prefix sums in A."""
+    n = len(A)
+    count = 0
+    for i in range(n):  # loop from 0 to n-1
+        total = 0
+        for j in range(n):  # loop from 0 to n-1
+            for k in range(1 + j):  # loop from 0 to j
+                total += A[k]
+        if B[i] == total:
+            count += 1
+    return count
