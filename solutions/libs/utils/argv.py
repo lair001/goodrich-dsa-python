4d678392f49b357094ac8f96c6ed2646adb23380
Samuel Leighton Lair <lair001@gmail.com>
2022-08-02 20:06:44 -0400

    Add more utils for parsing lists and matrices

--- solutions/libs/utils/argv.py
+++ solutions/libs/utils/argv.py
@@ -1,4 +1,13 @@
 import sys, re
+from collections import deque
+
+_openings = ['[', '{', '(', '<']
+_openings_to_closings = {
+    '[': ']',
+    '{': '}',
+    '(': ')',
+    '<': '>'
+}
 
 
 def parse_list(beginning_index):
@@ -6,13 +15,7 @@ def parse_list(beginning_index):
     current_index = beginning_index
     opening = None
     closing = None
-    openings = ['[', '{', '(', '<']
-    openings_to_closings = {
-        '[': ']',
-        '{': '}',
-        '(': ')',
-        '<': '>'
-    }
+
     blanks = [" ", ""]
 
     while True:
@@ -20,15 +23,17 @@ def parse_list(beginning_index):
         clean_arg = arg
         current_index += 1
         if opening is None:
-            if arg[0] in openings:
+            if arg[0] in _openings:
                 opening = arg[0]
                 clean_arg = re.sub(r"\A%s" % (re.escape(opening)), '', clean_arg)
-                closing = openings_to_closings[opening]
+                closing = _openings_to_closings[opening]
                 cleaning_pattern = r"(,|%s\Z)" % (re.escape(closing))
             else:
                 continue
         clean_arg = re.sub(cleaning_pattern, ' ', clean_arg)
         for token in clean_arg.split(" "):
+            if token in _openings:
+                return (None, current_index)
             if token not in blanks:
                 seq.append(token)
         if arg[-1] == closing:
@@ -37,5 +42,46 @@ def parse_list(beginning_index):
 
 
 def parse_int_list(beginning_index):
+    return _parse_list_of_type(beginning_index, int)
+
+
+def parse_float_list(beginning_index):
+    return _parse_list_of_type(beginning_index, int)
+
+
+def _parse_list_of_type(beginning_index, type):
     raw = parse_list(beginning_index)
-    return list(map(int, raw[0])), raw[1]
+    return raw if raw[0] is None else (list(map(type, raw[0])), raw[1])
+
+
+def parse_int_matrix(beginning_index):
+    return _parse_matrix_of_type(beginning_index, int)
+
+
+def _parse_matrix_of_type(beginning_index, type):
+    current_index = beginning_index
+    max_index = current_index
+    openings_info = []
+    lists = []
+    while True:
+        arg = sys.argv[current_index]
+        if arg[0] in _openings:
+            openings_info.append((arg[0], current_index))
+            lists.append([])
+            if len(lists) > 1:
+                lists[-2].append(lists[-1])
+        if arg[-1] == _openings_to_closings[openings_info[-1][0]]:
+            opening_index = openings_info.pop()[1]
+            curr, current_index = _parse_list_of_type(opening_index, type)
+            max_index = max(max_index, current_index)
+            if curr is None:
+                if len(lists) == 1:
+                    return lists[0], max_index + 1
+                else:
+                    lists.pop()
+                    current_index = max_index + 1
+            else:
+                lists.pop().extend(curr)
+        else:
+            current_index += 1
+            max_index = max(max_index, current_index)
--- tests/unit/libs/utils/test_argv.py
+++ tests/unit/libs/utils/test_argv.py
@@ -9,7 +9,7 @@ def execute_base_parse_list_test(args, expectations, parsing_function, starting_
         current_index = starting_index
         for expected_values in expectations:
             lst, current_index = parsing_function(current_index)
-            
+
             for i in range(len(expected_values)):
                 assert lst[i] is expected_values[i]
         assert current_index is len(args)
@@ -25,6 +25,10 @@ def execute_parse_list_test_with_starting_index(args, expected_values, starting_
 
 def execute_parse_int_list_test(args, expected_values):
     execute_base_parse_list_test(args, expected_values, argv_utils.parse_int_list, 1)
+
+# TODO: Work on how to run assertions on a matrix of arbitrary depth
+# def execute_parse_int_matrix_test(args, expected_values):
+#     execute_base_parse_list_test(args, expected_values, argv_utils.parse_int_matrix, 1)
 
 
 def test_parse_list_with_square_brackets():
@@ -44,10 +48,12 @@ def test_parse_list_with_parenthesis():
     expected_values = ["1", "2", "3"]
     execute_parse_list_test(args, expected_values)
 
+
 def test_parse_list_with_angular_brackets():
     args = ["path", "<", "1", "2", "3", ">"]
     expected_values = ["1", "2", "3"]
     execute_parse_list_test(args, expected_values)
+
 
 def test_parse_list_with_multiple_lists():
     args = ["path", "[", "1", "2", "3", "]", "[", "4", "5", "6", "]"]
@@ -72,6 +78,7 @@ def test_parse_list_with_list_as_single_arg_and_elements_separated_by_spaces():
     expected_values = ["1", "2", "3"]
     execute_parse_list_test(args, expected_values)
 
+
 def test_parse_list_with_list_as_single_arg_and_elements_separated_by_by_commas_and_spaces():
     args = ["path", "[ 1 , 2 , 3 ]"]
     expected_values = ["1", "2", "3"]
@@ -90,3 +97,14 @@ def test_parse_int_list_with_a_non_integer_in_list():
         with pytest.raises(ValueError) as ve:
             argv_utils.parse_int_list(1)
         assert "invalid literal for int() with base 10" in str(ve)
+
+
+# def test_parse_int_matrix():
+#     args = ["path", "[", "[", "1", "2", "]", "[", "3", "4", "]", "]"]
+#     expected_values = [[1, 2], [3, 4]]
+#     execute_parse_int_matrix_test(args, expected_values)
+
+# def test_parse_int_matrix():
+#     args = ["path", "[", "[", "[", "1", "2", "]", "]", "[", "[", "3", "4", "]", "]", "]"]
+#     expected_values = [[[1, 2]], [[3, 4]]]
+#     execute_parse_int_matrix_test(args, expected_values)
