ff46e397a2c194aa17cfc683ef78629f352ac177
Samuel Leighton Lair <lair001@gmail.com>
2022-08-08 19:52:10 -0400

    Add a util for getting canonical paths within this repo

---
+++ solutions/libs/utils/path.py
@@ -0,0 +1,4 @@
+from os import path as os_path
+
+PROJECT_ROOT = os_path.dirname(os_path.dirname(os_path.dirname(os_path.dirname(os_path.realpath(__file__)))))
+SOLUTIONS_CH1 = os_path.join(PROJECT_ROOT, "solutions", "exercises", "ch1")
