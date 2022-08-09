62469773d855f3b0f227af14fd40cbda1caaf883
Samuel Leighton Lair <lair001@gmail.com>
2022-08-09 18:39:40 -0400

    Add path utils for chapters 2-6

--- solutions/libs/utils/path.py
+++ solutions/libs/utils/path.py
@@ -1,4 +1,10 @@
 from os import path as os_path
 
 PROJECT_ROOT_PATH = os_path.dirname(os_path.dirname(os_path.dirname(os_path.dirname(os_path.realpath(__file__)))))
-SOLUTIONS_CH1_PATH = os_path.join(PROJECT_ROOT_PATH, "solutions", "exercises", "ch1")
+SOLUTIONS_EXERCISES_PATH = os_path.join(PROJECT_ROOT_PATH, "solutions", "exercises")
+SOLUTIONS_CH1_PATH = os_path.join(SOLUTIONS_EXERCISES_PATH, "ch1")
+SOLUTIONS_CH2_PATH = os_path.join(SOLUTIONS_EXERCISES_PATH, "ch2")
+SOLUTIONS_CH3_PATH = os_path.join(SOLUTIONS_EXERCISES_PATH, "ch3")
+SOLUTIONS_CH4_PATH = os_path.join(SOLUTIONS_EXERCISES_PATH, "ch4")
+SOLUTIONS_CH5_PATH = os_path.join(SOLUTIONS_EXERCISES_PATH, "ch5")
+SOLUTIONS_CH6_PATH = os_path.join(SOLUTIONS_EXERCISES_PATH, "ch6")
