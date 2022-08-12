88d34153e7c690d155517d2d06f9bc795f203750
Samuel Leighton Lair <lair001@gmail.com>
2022-08-11 21:12:14 -0400

    Adjust run aliases to be consistent with test aliases

--- aliases/util.py
+++ aliases/util.py
@@ -5,11 +5,13 @@ from os import path as os_path
 
 aliases = {}
 
+
 def get_alias_path(alias):
-    chapter = alias.split("_")[0]
+    chapter = alias.split("/")[0]
     load_aliases(chapter)
 
     return aliases[chapter][alias]
+
 
 def get_alias_list(chapter):
     load_aliases(chapter)
@@ -22,20 +24,22 @@ def get_alias_list(chapter):
 
     return result
 
+
 def add_alias(chapter, exercise, filename):
     if chapter not in aliases.keys():
         aliases[chapter] = {}
 
     i = 1
     while True:
-        alias = "{}_{}".format(chapter, exercise)
+        alias = "{}/{}".format(chapter, exercise)
         if i > 1:
-            alias = "{}_{}".format(alias, i)
+            alias = "{}/{}".format(alias, i)
         if alias not in aliases[chapter].keys():
             aliases[chapter][alias] = os_path.join("solutions", "exercises", chapter, exercise, filename)
             return alias
         else:
             i += 1
+
 
 def load_aliases(chapter):
     if len(chapter) < 3:
